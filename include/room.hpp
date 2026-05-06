fde06ac28176a8b04a14727a89f5d140bf16b723
CodeByisaac <highzik86@gmail.com>
2026-05-06 19:46:59 -0400

    add room.hpp

---
+++ include/room.hpp
@@ -0,0 +1,16 @@
+#pragma once
+#include <set>
+#include <memory>
+#include <string>
+
+class Session;
+
+class Room {
+public:
+  void join(std::shared_ptr<Session> session);
+  void leave(std::shared_ptr<Session> session);
+  void broadcast(const std::string& message);
+
+private:
+  std::set<std::shared_ptr<Session>> sessions_;
+};
