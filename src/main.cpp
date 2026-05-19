e5ac60be14f27bd600849fcce89bda2a8fd3fc7a
CodeByisaac <highzik86@gmail.com>
2026-05-18 22:01:54 -0400

    add multithreading with thread pool

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,16 @@ int main() {
     Server server(io, 8080);
     server.start_accept();
 
-    io.run();
+    //io.run();
+
+    std::vector<std::thread> threads;
+    for (int i = 0; i < std::thread::hardware_concurrency(); ++i) {
+      threads.emplace_back([&io]{io.run();});
+    }
+
+    for (auto &t : threads) {
+      t.join();
+    }
     
   } catch (const std::exception& e) {
     std::cerr << "Error: " << e.what() << std::endl;
