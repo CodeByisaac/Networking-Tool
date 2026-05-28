17f2640618e88d21cb65a640f098f0666b17022e
CodeByisaac <highzik86@gmail.com>
2026-05-28 02:31:19 -0400

    Add ThreadSanitizer for debugging data race

---
+++ CMakeFiles/Networking-Tool.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/mnt/f/projects/Networking-Tool/src/main.cpp" "CMakeFiles/Networking-Tool.dir/src/main.cpp.o" "gcc" "CMakeFiles/Networking-Tool.dir/src/main.cpp.o.d"
+  "/mnt/f/projects/Networking-Tool/src/room.cpp" "CMakeFiles/Networking-Tool.dir/src/room.cpp.o" "gcc" "CMakeFiles/Networking-Tool.dir/src/room.cpp.o.d"
+  "/mnt/f/projects/Networking-Tool/src/server.cpp" "CMakeFiles/Networking-Tool.dir/src/server.cpp.o" "gcc" "CMakeFiles/Networking-Tool.dir/src/server.cpp.o.d"
+  "/mnt/f/projects/Networking-Tool/src/session.cpp" "CMakeFiles/Networking-Tool.dir/src/session.cpp.o" "gcc" "CMakeFiles/Networking-Tool.dir/src/session.cpp.o.d"
+  "" "Networking-Tool" "gcc" "CMakeFiles/Networking-Tool.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/Networking-Tool.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Networking-Tool.dir/link.d"
+  "CMakeFiles/Networking-Tool.dir/src/main.cpp.o"
+  "CMakeFiles/Networking-Tool.dir/src/main.cpp.o.d"
+  "CMakeFiles/Networking-Tool.dir/src/room.cpp.o"
+  "CMakeFiles/Networking-Tool.dir/src/room.cpp.o.d"
+  "CMakeFiles/Networking-Tool.dir/src/server.cpp.o"
+  "CMakeFiles/Networking-Tool.dir/src/server.cpp.o.d"
+  "CMakeFiles/Networking-Tool.dir/src/session.cpp.o"
+  "CMakeFiles/Networking-Tool.dir/src/session.cpp.o.d"
+  "Networking-Tool"
+  "Networking-Tool.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Networking-Tool.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
